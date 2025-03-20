e2bbc507edff9c283cf1675008cdbde18e00dab6
= <=>
2025-03-20 21:14:21 +0300

    Update StoryLengthEnum.cs

--- StoryApp/Domain/Enums/StoryLengthEnum.cs
+++ StoryApp/Domain/Enums/StoryLengthEnum.cs
@@ -2,7 +2,7 @@
 
 public enum StoryLengthEnum
 {
-    FlashFiction,  // قصة قصيرة جدًا (أقل من 1000 كلمة)
+    FlashStory,  // قصة قصيرة جدًا (أقل من 1000 كلمة)
     ShortStory,    // قصة قصيرة (1000 - 7500 كلمة)
     Novella,       // رواية قصيرة (7500 - 40000 كلمة)
 }
